9d10f2dc1f25d6d0e70beabf513ba8c7e01e687a
AuraSalomaa <aura@salomaa.info>
2023-08-22 14:11:40 +0300

    tehtävä 1 tehty

--- 2tehtävä moduuli1.py
+++ 2tehtävä moduuli1.py
@@ -1,3 +1,10 @@
 name = input("Mikä on nimesi?")
+circle = int(input("Syötä ympyrän säde:"))
+width = int(input("Syötä leveys:"))
+hight = int(input("syötä korkeus:"))
+CircleArea = circle * circle * 3.14
+squareArea = width * hight
+squareAround = (width * width) + (hight + hight)
 
-print(f"Terve {name}!")
+print(f"Terve {name}!. {CircleArea}. {squareArea} ja {squareAround}")
+
