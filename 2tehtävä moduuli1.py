1c9207922bb36467f72d306d12e6e5d831009b06
AuraSalomaa <aura@salomaa.info>
2023-08-23 10:07:41 +0300

    tehtävä 1 tehty

--- 2tehtävä moduuli1.py
+++ 2tehtävä moduuli1.py
@@ -5,16 +5,20 @@ hight = int(input("syötä korkeus:"))
 a = int(input("anna ensimmäinen numero:"))
 b = int(input("anna toinen numero:"))
 c = int(input("anna kolmas numero:"))
+luodit = float(input("anna luodit:"))
+naulat = int(input("anna naulat:"))
+leiviskat = int(input("anna leiviskat:"))
 CircleArea = circle * circle * 3.14
 squareArea = width * hight
 squareAround = (width * width) + (hight + hight)
 keskiarvo = a + b + c /3
 summa = a + b + c
 tulo = a * b * c
-sievennetty = round(keskiarvo)
+sievennetty = (keskiarvo)
 luoti = 13.33
 naula = 32 * luoti
-leiviskä = naula * 20
-
-print(f"Terve {name}!. {CircleArea}. {squareArea} ja {squareAround}.{sievennetty} {summa} {tulo}. ")
+leiviska = naula * 20
+lasku = (leiviska * leiviskat) + (naula * naulat) + (luoti * luodit)
+print(round(lasku, 2))
+#print(f"Terve {name}!. {CircleArea}. {squareArea} ja {squareAround}.{sievennetty} {summa} {tulo}. ")
 
