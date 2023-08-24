608f94e3f49d64f5462c69f6330ce2545f9a0fa2
AuraSalomaa <aura@salomaa.info>
2023-08-24 09:43:50 +0300

    tehtävä 1 tehty

--- 2tehtävä moduuli1.py
+++ 2tehtävä moduuli1.py
@@ -13,19 +13,21 @@ leiviskat = int(input("anna leiviskat:"))
 kolmenumero_koodi = random.sample(range(1,9),3)
 nelinumero_koodi  = random.sample(range(1,6),4)
 CircleArea = circle * circle * math.pi
+SievennettyCircle = "{:.2f}".format(CircleArea)
 squareArea = width * hight
 squareAround = (width * width) + (hight + hight)
 keskiarvo = a + b + c /3
 summa = a + b + c
 tulo = a * b * c
-sievennetty = (keskiarvo)
+SievennettyKeskiarvo = "{:.2f}".format(keskiarvo)
 luoti = 13.30
 naula = 32 * luoti
 leiviska = naula * 20
 lasku = (leiviska * leiviskat) + (naula * naulat) + (luoti * luodit)
 osa = '{:,}'.format(lasku)
+
 osat = osa.split(",",1 )
-print(f"Terve {name}!ln.Antamasi ympyrän mitan pinta-ala on {CircleArea}.ln Suorakulmiosi pinta-ala on{squareArea} ja suorakulmion piiri.{squareAround}.")
-print(f"antamiesi numeroiden keskiarvo{sievennetty}, summa {summa} ja tulo ovat tässä{tulo}.Kilogrammaa {osat[0]}, grammaa {osat[1]} ")
-print(f"Antamiesi tietojen nykyinen paino on {osat[0]} kg ja {osat[1]} grammaa")
+print(f"Terve {name}!\nAntamasi ympyrän mitan pinta-ala on {CircleArea} ja sievennetty {SievennettyCircle}.\nSuorakulmiosi pinta-ala on {squareArea} ja suorakulmion piiri {squareAround}.")
+print(f"Antamiesi numeroiden keskiarvo {keskiarvo} ja sievennetysti {SievennettyKeskiarvo}, summa {summa} ja tulo ovat tässä {tulo}.")
+print(f"Antamiesi tietojen nykyinen paino on {osat[0]} kg ja {osat[1]} grammaa.")
 print(f"Lopuksi tässä sinulle tarvittavat koodit numero lukkoihin kolmenumeroinen {kolmenumero_koodi} ja lisäksi nelinumeroinen koodi {nelinumero_koodi}")
