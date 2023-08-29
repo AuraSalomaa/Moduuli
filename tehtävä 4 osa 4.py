38de07e90cbe27760c4bb3c50e74daea6c7d6fea
AuraSalomaa <aura@salomaa.info>
2023-08-29 13:44:37 +0300

    tehtävä 4 osa 4 valmis

--- tehtävä 4 osa 4.py
+++ tehtävä 4 osa 4.py
@@ -2,8 +2,10 @@ import random
 arvo = random.randint(1, 10)
 while True:
     luku = int(input("Arvaa luku: "))
-    if arvo != luku:
-        print("Yritä uudestaan..")
+    if luku < arvo:
+        print("Liian pieni arvaus..")
+    elif luku > arvo:
+        print("Liian suuri arvaus..")
     else:
         break
 print("Oikein!")
