ad3e3abdd0c92767ffcf63b4293c8d53cb519159
AuraSalomaa <aura@salomaa.info>
2023-09-04 20:34:30 +0300

    tehtävä 5 osa 3 valmis

--- tehtävä 5 osa 4.py
+++ tehtävä 5 osa 4.py
@@ -1,1 +1,9 @@
-print("F")
+kaupunki_lista = []
+for i in range(5):
+    syote = input("Syötä kaupungin nimet:")
+    kaupunki_lista.append(syote)
+index = 0
+for i in kaupunki_lista:
+
+    print(kaupunki_lista[index])
+    index += 1
