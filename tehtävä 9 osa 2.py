583df675307cf5cf5f95d3900d343f78e715a096
AuraSalomaa <aura@salomaa.info>
2023-10-23 17:10:34 +0300

    tehtävä 9 osa 2.py

---
+++ tehtävä 9 osa 2.py
@@ -0,0 +1,16 @@
+class Auto:
+    def __init__(self, rekisteri, huippunopeus, nopeus, ajomaara, kiihdyta):
+        self.rekisteri = rekisteri
+        self.huippunopeus = huippunopeus
+        self.nopeus = nopeus
+        self.ajomaara = ajomaara
+        self.kiihdyta = kiihdyta
+
+
+    def self.nopeus:
+
+auto1 = Auto("ABC-123", 142, 0, 0, 40)
+auto1.nopeus += auto1.kiihdyta
+
+print(f"rekisteriosoite on {auto1.rekisteri} ja huippunopeus on {auto1.huippunopeus} km/h")
+print(f"nopeus kiihtyi {auto1.kiihdyta} ja nopeus on {auto1.nopeus}")
