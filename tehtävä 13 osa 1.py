fc609b9ea7d6e964688f4846f476279bdc0c4274
AuraSalomaa <aura@salomaa.info>
2023-11-13 09:57:14 +0200

    12 osa 2 valmis

--- tehtävä 13 osa 1.py
+++ tehtävä 13 osa 1.py
@@ -1,4 +1,30 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-@app.route('')
+
+
+@app.route('/PrimeNumber')
+def alku_luku():
+    args = request.args
+    number = int(args.get("number"))
+    prime = 0
+
+    if number > 1:
+        for i in range(2, number):
+
+            if number % i == 0:
+                prime = False
+                break
+        else:
+            prime = True
+
+    vastaus = {
+        "Number": number,
+        "IsPrime": prime
+
+    }
+    return str(vastaus)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port= 3000)
