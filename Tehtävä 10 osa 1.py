099189241436723327786c9752e3774d67386e41
AuraSalomaa <aura@salomaa.info>
2023-11-21 12:10:42 +0200

    Tehtävä 10 osa 1 valmis

--- Tehtävä 10 osa 1.py
+++ Tehtävä 10 osa 1.py
@@ -1,41 +1,32 @@
 class Hissi:
+    kysynta = input("Up or down ?")
 
     def __init__(self, alinkerros, ylinkerros):
         self.alinkerros = alinkerros
         self.ylinkerros = ylinkerros
 
-    def siirry_kerrokseen(self, kerros):
-        self.kerros = kerros
 
-        return kerros
 
     def kerros_ylos(self):
-        ylos = 0
-        for i in range(self.alinkerros, self.kerros):
-            if ylos != self.ylinkerros:
-                ylos += 1
-                print(ylos)
 
-            else:
-                print(self.ylinkerros)
+        return + 1
 
 
     def kerros_alas(self):
-        alas = self.ylinkerros
-        for i in range(self.ylinkerros, self.kerros, -1):
-            if alas != self.alinkerros:
-                alas -= 1
-                print(alas)
-            else:
-                print(self.alinkerros)
 
+        return - 1
 
+    def siirry_kerrokseen(self, kerros):
+        if Hissi.kysynta == "up":
+            for i in range(kerros):
+                print(i + self.kerros_ylos())
+
+        elif Hissi.kysynta == "down":
+            for i in range(kerros, self.alinkerros, -1):
+                print(i - self.kerros_alas())
 
 
 
 
 h = Hissi(0,10)
-
-
-kerroksia = h.siirry_kerrokseen(5)
-h.kerros_alas()
+kerroksia = h.siirry_kerrokseen(6)
